6d0e4a6ccd5b11726a95c8883cb81b0d5139eb21
adamjohnson187 <johnsonadam187@gmail.com>
2024-11-12 23:13:59 +1300

    resave

--- Create_tables.sql
+++ Create_tables.sql
@@ -20,7 +20,7 @@ CREATE TABLE PRODUCTS(
   product_id SERIAL PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   description TEXT NULL,
-  price DECIMAL(10,2) NOT NULL
+  price DECIMAL(10,2) NOT NULL,
   stock_quantity INT NOT NULL
 );
 
